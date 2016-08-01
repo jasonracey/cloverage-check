2be606fe50fd499752fd010f72b21f2b3adb35c8
Jason Racey <jason.racey@base2s.com>
2016-08-01 15:48:05 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject cloverage-check "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-cloverage "1.0.6"]])
---
+++ src/cloverage_check/math.clj
@@ -0,0 +1,12 @@
+(ns cloverage-check.math)
+
+; NOTE - report output for code below
+; Forms covered: 33.33 %
+; Lines covered: 100.00 %
+; (defn add-one [arg] (+ 1 arg))
+
+; NOTE - report output for code below
+; Forms covered: 33.33 %
+; Lines covered: 66.67 %
+(defn add-one [arg]
+	(+ 1 arg))
